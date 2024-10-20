bb959a24b5979d8d1fa95cb265be017eeef32cde
Godani05 <ezechiel.huego@epitech.eu>
2024-10-20 23:20:27 +0000

    [UPDATE0] portofolio modification and addition

--- Portfolio_HD/src/app/components/HeroSection.jsx
+++ Portfolio_HD/src/app/components/HeroSection.jsx
@@ -41,14 +41,14 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="https://wa.me/+22557379276"
               // href="https://wa.me/+22557379276"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-400 to-blueGray-600 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
-              href="/Cv/Cv E_Huego.pdf"
+              href="/Cv/Cv E_Huego .pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br  from-blue-400 to-blueGray-600 hover:bg-slate-800 text-white mt-3"
               download
             >
